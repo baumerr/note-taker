c072ee6ac95e996227c33bf0e021eb9d88cf485d
randy <baumer.randy@gmail.com>
2021-11-07 11:00:37 -0500

    added ability to click the left side and see note on the right side

--- server.js
+++ server.js
@@ -1,54 +1,62 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const notes = require('./Develop/db/db.json');
+const notes = require("./Develop/db/db.json");
 
 // Use css and js
-app.use(express.static('Develop/public'));
+app.use(express.static("Develop/public"));
 
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // Parse incoming Json
 app.use(express.json());
 
-
 // INDEX ROUTES START
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
 // INDEX ROUTES END
 
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-})
+// show all notes from db.json in the notes section
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
-app.post('/api/notes', (req, res) => {
-    // store user input in variable
-    var newNote = JSON.stringify(req.body);
-    
-    // push that input into the already created notes
-    newNote = JSON.parse(newNote);
-    const pastNotes = notes;
-    pastNotes.push(newNote);
+// add notes from user input
+app.post("/api/notes", (req, res) => {
 
-    // write the db.json with all current notes
-    fs.writeFile('./Develop/db/db.json', JSON.stringify(pastNotes), err => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('written');
-        }
-    });
-    res.send('success');
-})
+  req.body.id = notes.length.toString();
+
+  // store user input in variable
+  var newNote = JSON.stringify(req.body);
+
+  // push that input into the already created notes
+  newNote = JSON.parse(newNote);
+  const pastNotes = notes;
+  pastNotes.push(newNote);
+
+  // write the db.json with all current notes
+  fs.writeFile("./Develop/db/db.json", JSON.stringify(pastNotes), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("written");
+    }
+  });
+  res.send("success");
+});
+
+// app.get("/api/notes:id", (req, res) => {
+//   console.log(req.body.id);
+// });
 
 app.listen(PORT, () => {
-    console.log(`Listening on Port ${PORT}!`);
-})
+  console.log(`Listening on Port ${PORT}!`);
+});
